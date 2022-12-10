47f8f56e508a9f9fe2758bfa30a87742499412e5
Uenderss <meajudabarto@gmail.com>
2022-12-10 03:12:49 -0300

    feat: insert new erros

--- src/errors/not-found-error.ts
+++ src/errors/not-found-error.ts
@@ -1,6 +1,6 @@
 import { ApplicationError } from "@/protocols";
 
-export function   notFoundError(): ApplicationError {
+export function  notFoundError(): ApplicationError {
   return {
     name: "NotFoundError",
     message: "No result for this search!",
