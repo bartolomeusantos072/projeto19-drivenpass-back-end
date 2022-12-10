0ca080190e9f0fdd469639609c7bd07a0fd39704
Uenderss <meajudabarto@gmail.com>
2022-12-10 03:15:10 -0300

    feat: insert utils crypt

---
+++ src/utils/cryptrUtils.ts
@@ -0,0 +1,13 @@
+import Cryptr from "cryptr";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env" });
+
+const cryptr = new Cryptr(process.env.CRYPTR_SECRET);
+
+export function encrypt(value: string) {
+  return cryptr.encrypt(value);
+}
+
+export function decrypt(encryptedValue: string) {;
+  return cryptr.decrypt(encryptedValue);
+}
